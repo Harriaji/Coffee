729f2202be1b41ea00b48625ec74a3494acee8cd
zenosamaaa <rezalol333@gmail.com>
2023-06-19 15:55:28 +0700

    Commit Project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// toogle Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik Outside Sidebar
+
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
